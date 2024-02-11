ab672f647929991726d7833834672ff034a86c82
ElenaNec <nechaeva_sam@mail.ru>
2024-02-11 18:09:47 +0300

    initial commit

---
+++ task2/task2.py
@@ -0,0 +1,80 @@
+# Задание 2
+# Напишите программу, которая рассчитывает положение точки относительно
+# окружности.
+# Координаты центра окружности и его радиус считываются из файла 1.
+# Пример:
+# 1 1
+# 5
+# Координаты точек считываются из файла 2.
+# Пример:
+# 0 0
+# 1 6
+# 6 6
+# Вывод для данных примеров файлов:
+# 1
+# 0
+# 2
+# Пути к файлам передаются программе в качестве аргументов!
+# ● файл с координатами и радиусом окружности - 1 аргумент;
+# ● файл с координатами точек - 2 аргумент;
+# ● координаты - рациональные числа в диапазоне от 10-38 до 1038;
+# ● количество точек от 1 до 100;
+# ● вывод каждого положения точки заканчивается символом новой строки;
+# ● соответствия ответов:
+# ○ 0 - точка лежит на окружности
+# ○ 1 - точка внутри
+# ○ 2 - точка снаружи.
+# Вывод программы в консоль.
+
+import sys
+
+
+def read_circle(filename):
+    try:
+        with open(filename, 'r') as file:
+            x, y = map(float, file.readline().split())
+            radius = float(file.readline())
+        return x, y, radius
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        sys.exit(1)
+    except ValueError:
+        print(f"Error reading data from {filename}.")
+        sys.exit(1)
+
+def read_points(filename):
+    points = []
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                x, y = map(float, line.split())
+                points.append((x, y))
+        return points
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        sys.exit(1)
+    except ValueError:
+        print(f"Error reading data from {filename}.")
+        sys.exit(1)
+
+def point_position(x_center, y_center, radius, x_point, y_point):
+    distance_squared = (x_point - x_center) ** 2 + (y_point - y_center) ** 2
+    if distance_squared < radius ** 2:
+        return 1  # Точка внутри окружности
+    elif distance_squared > radius ** 2:
+        return 2  # Точка снаружи окружности
+    else:
+        return 0  # Точка на окружности
+
+
+if __name__ == "__main__":
+
+    circle_file = input('Input path "circle_file": ')
+    points_file = input('Input path "points_file": ')
+
+    x_center, y_center, radius = read_circle(circle_file)
+    points = read_points(points_file)
+
+    for point in points:
+        position = point_position(x_center, y_center, radius, *point)
+        print(position)
---
+++ task3/task3.py
@@ -0,0 +1,52 @@
+# Задание 3
+# На вход в качестве аргументов программы поступают три пути к файлу (в приложении
+# к заданию находятся примеры этих файлов):
+# ● values.json содержит результаты прохождения тестов с уникальными id
+# ● tests.json содержит структуру для построения отчета на основе прошедших
+# тестов (вложенность может быть большей, чем в примере)
+# ● report.json - сюда записывается результат.
+# Напишите программу, которая формирует файл report.json с заполненными полями
+# value для структуры tests.json на основании values.json.
+# Структура report.json такая же, как у tests.json, только заполнены поля “value”.
+# На вход программы передается три пути к файлу!
+
+import json
+import sys
+
+def update_report_with_values(values_path, tests_path, report_path):
+    # Загрузка данных из файлов
+    try:
+        with open(values_path, 'r') as values_file:
+            values_data = json.load(values_file)
+    except FileNotFoundError:
+        print(f"File {values_path} not found.")
+        sys.exit(1)
+
+    try:
+        with open(tests_path, 'r') as tests_file:
+            tests_data = json.load(tests_file)
+    except FileNotFoundError:
+        print(f"File {tests_path} not found.")
+        sys.exit(1)
+
+    # Функция для обновления значений в тестах
+    def update_values(tests, values):
+        for test_item in tests:
+            test_id = test_item.get('id')
+            test_item['value'] = next((value['value'] for value in values if value['id'] == test_id), "")
+            if 'values' in test_item:
+                update_values(test_item['values'], values)
+
+    update_values(tests_data['tests'], values_data['values'])
+
+    # Запись обновленных данных в файл report.json
+    with open(report_path, 'w') as report_file:
+        json.dump(tests_data, report_file, indent=4)
+
+
+if __name__ == "__main__":
+    values_path = input('Input path "values.json": ')
+    tests_path = input('Input path "tests.json": ')
+    report_path = input('Input path "report.json": ')
+
+    update_report_with_values(values_path, tests_path, report_path)
